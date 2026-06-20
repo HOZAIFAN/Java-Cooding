ad0bf86d59da66c0419bceb1c604599c067a2671
MUHAMMAD HOZAIFA NAEEM <ssjutt2023@gmail.com>
2026-06-20 12:01:01 +0500

    Add files via upload

---
+++ Java-Projects/Project Wheather Application/Code/WeatherAppGUI-Java-main/src/Filehandling.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Filehandling {
+    public static void main(String[] args) {
+       try{
+       Scanner sc=new Scanner(System.in);
+           System.out.println("How many Characters You want to Enter");
+           int n=sc.nextInt();
+           int[]   arry=new int[n-1];
+           for (int i=0;i<n;i++){
+           String st=sc.nextLine();
+           char ch=st.charAt(0);
+           
+           }
+       }
+    }
+}
